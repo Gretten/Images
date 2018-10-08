7f1524c4f0bab76c5059df5b03672f4c694323e3
d.shch <d.sherbakov@admatix.com>
2018-10-08 10:25:05 +0300

    Readme change

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Imgrinder 0.7
-This is a simple images changer based on Gulp task manager.
+This is a simple image handler based on Gulp task runner.
 What can it do:
-- optimisate all pngs, jpegs, whatever with one "gulp images" command;
+- optimize all pngs, jpegs, whatever with one "gulp images" command;
 - size changing ("gulp resize");
 - creating of sprites without any external services ("gulp sprite").
 
 # Install
 
-1. Clone this little piece of chaos in your project folder;
+1. Clone this little piece of chaos in some folder;
 2. Type "npm install" in the command line;
-3. Open gulpfile.js and change variables in its command line section;
+3. Open gulpfile.js and change variables in its Control panel section;
 4. Enjoy!
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 let gulp = require('gulp');
-let imagemin = require('gulp-imagemin'); 
+let imagemin = require('gulp-imagemin');
 let imageminJpegRecompress = require('imagemin-jpeg-recompress');
 let pngquant = require('imagemin-pngquant');
 let cache = require('gulp-cache');
@@ -7,56 +7,58 @@ let spritesmith = require('gulp.spritesmith');
 let resizer = require('gulp-images-resizer');
 
 // **** Control panel **** //
+
 let padding = 30,
     width = 50,
     height = 50
+
 // **** ************* **** //
 
 // Images optimization and copy in /dist
 gulp.task('images', function() {
-  return gulp.src('app/img/*')
-    .pipe(cache(imagemin([
-      imagemin.gifsicle({interlaced: true}),
-      imagemin.jpegtran({progressive: true}),
-      imageminJpegRecompress({
-        loops: 5,
-        min: 65,
-        max: 70,
-        quality:'medium'
-      }),
-      imagemin.svgo(),
-      imagemin.optipng({optimizationLevel: 3}),
-      pngquant({quality: '65-70', speed: 5})
-    ],{
-      verbose: true
-    })))
-    .pipe(gulp.dest('dist/images'));
+    return gulp.src('app/img/*')
+        .pipe(cache(imagemin([
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.jpegtran({ progressive: true }),
+            imageminJpegRecompress({
+                loops: 5,
+                min: 65,
+                max: 70,
+                quality: 'medium'
+            }),
+            imagemin.svgo(),
+            imagemin.optipng({ optimizationLevel: 3 }),
+            pngquant({ quality: '65-70', speed: 5 })
+        ], {
+            verbose: true
+        })))
+        .pipe(gulp.dest('dist/images'));
 });
 
 // Clearing the cache
-gulp.task('clear', function (done) {
-  return cache.clearAll(done);
+gulp.task('clear', function(done) {
+    return cache.clearAll(done);
 });
 
-  // Options: https://github.com/twolfson/gulp.spritesmith
-  gulp.task('sprite', () => {
+// Options: https://github.com/twolfson/gulp.spritesmith
+gulp.task('sprite', () => {
     let spriteData =
-      gulp.src('app/img/sprites/*.*') 
-      .pipe(spritesmith({
-        imgName: 'sprite.png',
-        cssName: 'sprite.css',
-        padding: padding,
-        algorithm: 'binary-tree',
-    }));
+        gulp.src('app/img/sprites/*.*')
+        .pipe(spritesmith({
+            imgName: 'sprite.png',
+            cssName: 'sprite.css',
+            padding: padding,
+            algorithm: 'binary-tree',
+        }));
     spriteData.img.pipe(gulp.dest('dist/sprite'));
     spriteData.css.pipe(gulp.dest('dist/sprite'));
-  })
-  
-  gulp.task('resize', function() {
-      return gulp.src('app/img/resize/*.*')
-          .pipe(resizer({
-              width: width,
-              height: height
-          }))
-      .pipe(gulp.dest('dist/resized'));
-  });
+})
+
+gulp.task('resize', function() {
+    return gulp.src('app/img/resize/*.*')
+        .pipe(resizer({
+            width: width,
+            height: height
+        }))
+        .pipe(gulp.dest('dist/resized'));
+});
